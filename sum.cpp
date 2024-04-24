7d741117a764a201a418732bba0a306010ab304e
Do Thanh Lam <120783677+thanhlam30@users.noreply.github.com>
2024-04-24 10:37:02 +0700

    No commir

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Nhap a: ";
+    cin>>a;
+    cout<<"\nNhap b: ";
+    cin>>b;
+    cout<<"Tong a + b ="<<a+b;
+    return 0;
+}
